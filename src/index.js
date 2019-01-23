45391d4135b4acc131561e3cb664eb6105e3e05e
HebeHealer <HebeHealer@xgimi.com>
2019-01-23 21:38:36 +0800

    feat(项目初始化):兄弟组件 数据校验

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import PropTypes from 'prop-types';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 const myStyle = {
@@ -15,18 +16,36 @@ const arr = [
     "Spark"
 ];
 
+const state = {
+    name: 'zhongmin.gao'
+};
+
 class Clock extends React.Component{
     render() {
         return <h1 data-myattribute="1213i3" style={ myStyle }>hello world { arr } 现在是 {this.props.date.toLocaleTimeString()}!!!</h1>;
     }
 }
 
-function trick() {
-    ReactDOM.render(
-        <Clock date={ new Date() }/>,
-        document.getElementById('example'));
+class Student extends React.Component{
+    render() {
+        return (<h2>{ this.props.name }</h2>);
+    }
 }
-setInterval(trick, 1000);
+
+Student.propTypes = {
+    name: PropTypes.string
+};
+
+function Trick() {
+    return (
+        <div>
+            <Clock date={ new Date() }/>
+            <Student name={ state.name }/>
+        </div>
+    )
+}
+
+ReactDOM.render(<Trick/>, document.getElementById('example'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
