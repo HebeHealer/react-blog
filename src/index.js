164d22f1ab9f6d0e26d85e40c0951b66bd2b54ba
XGIMI\zhongmin.gao <zhongmin.gao@xgimi.com>
2019-01-24 11:03:45 +0800

    feat(项目初始化):事件处理 this绑定

--- src/index.js
+++ src/index.js
@@ -22,13 +22,36 @@ const state = {
 
 class Clock extends React.Component{
     render() {
-        return <h1 data-myattribute="1213i3" style={ myStyle }>hello world { arr } 现在是 {this.props.date.toLocaleTimeString()}!!!</h1>;
+        return <h1 data-myattribute="1213i3" style={ myStyle } >hello world { arr } 现在是 {this.props.date.toLocaleTimeString()}!!!</h1>;
     }
 }
 
 class Student extends React.Component{
     render() {
         return (<h2>{ this.props.name }</h2>);
+    }
+}
+
+class Teacher extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = { isToggleOn: true };
+        // 这边绑定是必要的，这样 `this` 才能在回调函数中使用
+        // this.handleClick = this.handleClick.bind(this);
+    }
+    handleClick(name) {
+        this.setState((preState) => ({
+            isToggleOn: !preState.isToggleOn
+        }));
+        alert(name);
+    }
+    render() {
+        return (
+            <div>
+                <button onClick={ this.handleClick.bind(this,'通过 bind() 方法传递参数') }> { this.state.isToggleOn ? 'ON' : 'OFF' } </button>
+            </div>
+
+        );
     }
 }
 
@@ -39,6 +62,7 @@ Student.propTypes = {
 function Trick() {
     return (
         <div>
+            <Teacher/>
             <Clock date={ new Date() }/>
             <Student name={ state.name }/>
         </div>
